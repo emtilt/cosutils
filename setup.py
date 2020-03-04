41f5bd54b4365de6e05555ffc0ce8a95f6e9e763
Evan M Tilton <emtilt@gmail.com>
2020-03-04 15:10:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='python-coaddx1d',
+setup(name='cosutils',
       version='0.1.0',
-      description='python-coaddx1d - generalized coaddition for COS x1d files',
-      author='Nico Nell',
-      author_email='nicholas.nell@colorado.edu',
-      url='http://casa.colorado.edu/~nell/site/software/python-coaddx1d.html',
+      description='cosutils - generalized coaddition and related utils for COS x1d files, forked from python-coaddx1d',
+      author='Nico Nell, Evan Tilton',
+      author_email='nicholas.nell@colorado.edu, etiltont@regis.edu',
+      url='https://github.com/emtilt/cosutils',
       packages=['coaddx1d'],
      )
